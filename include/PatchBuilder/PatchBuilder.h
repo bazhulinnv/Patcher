3f5c75e792df61f8551ff46955e5ed198ff88d25
NBazhulin <nikolay.bazhulin@lanit-tercom.com>
2019-07-03 11:00:43 +0300

    Create project Patcher

---
+++ include/PatchBuilder/PatchBuilder.h
@@ -0,0 +1,10 @@
+#ifndef PATCHBUILDER_H
+#define PATCHBUILDER_H
+#include <DBProvider/DBProvider.h>
+
+class PatchBuilder {
+public:
+  PatchBuilder();
+  ~PatchBuilder();
+};
+#endif
---
+++ src/PatchInstaller_exe.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+#include <PatchInstaller/PatchInstaller.h>
+
+int main() { std::cout << "hello world" << std::endl; }
