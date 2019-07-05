3285e8010ed221f5000335bce1f63584a4611e71
TimurSirkin <timur_sirkin@mail.ru>
2019-07-05 22:36:34 +0300

    Add temp params

--- src/PatchBuilder_exe.cpp
+++ src/PatchBuilder_exe.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
-//#include <PatchBuilder/PatchBuilder.h>
+#include <string>
+#include <PatchBuilder/PatchBuilder.h>
 
-int main() { std::cout << "hello world master" << std::endl; }
+using namespace std;
+
+int main() 
+{
+	string patchListDirectory; // Directory of patch list - a text file containing all the objects that need to be included in the patch
+	string objectListDirectory; // Directory of object list - text file containing all database objects
+	string installPocketDirectory; // Directory of install pocket - directory containing all the files needed to install the patch
+
+	cout << "Hello!";
+}
