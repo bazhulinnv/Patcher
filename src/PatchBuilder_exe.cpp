604fd9b788898d1518a8b69882372d283edc8246
TimurSirkin <timur_sirkin@mail.ru>
2019-07-07 16:36:15 +0300

    Create business logic

--- src/PatchBuilder_exe.cpp
+++ src/PatchBuilder_exe.cpp
@@ -6,9 +6,7 @@ using namespace std;
 
 int main() 
 {
-	string patchListDirectory; // Directory of patch list - a text file containing all the objects that need to be included in the patch
-	string objectListDirectory; // Directory of object list - text file containing all database objects
-	string installPocketDirectory; // Directory of install pocket - directory containing all the files needed to install the patch
-
-	cout << "Hello!";
+	string tempDirectory = "C:\\Users\\Timur\\Documents\\Temp";
+	PatchBuilder patchBuilder(tempDirectory);
+	patchBuilder.buildPatch(tempDirectory);
 }
