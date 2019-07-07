24ffb39e8487cb957e46c3dbd92b44143c89165a
TimurSirkin <timur_sirkin@mail.ru>
2019-07-07 16:36:15 +0300

    Create business logic

--- src/PatchBuilder.cpp
+++ src/PatchBuilder.cpp
@@ -1,5 +1,40 @@
 #include <DBProvider/DBProvider.h>
 #include <PatchBuilder/PatchBuilder.h>
 
-PatchBuilder::PatchBuilder() {}
+#include <iostream>
+
+using namespace std;
+
+PatchBuilder::PatchBuilder(string pPatchListDirectory)
+{
+	patchListDirectory = pPatchListDirectory;
+}
 PatchBuilder::~PatchBuilder() {}
+
+void PatchBuilder::buildPatch(string directory)
+{
+	createInstallPocket(directory);
+	createInstallScript(directory);
+	createDependencyList(directory);
+	cout << "Patch builded!" << endl;
+}
+
+void PatchBuilder::createInstallPocket(string directory)
+{
+	cout << "Install pocket created" << endl;
+}
+
+void PatchBuilder::createObjectList(string directory)
+{
+	cout << "Object list created" << endl;
+}
+
+void PatchBuilder::createInstallScript(string directory)
+{
+	cout << "Install script created" << endl;
+}
+
+void PatchBuilder::createDependencyList(string directory)
+{
+	cout << "Dependency list created" << endl;
+}
