bd2169938fedbb2d4eca350f240d96206c95b0bb
TimurSirkin <timur_sirkin@mail.ru>
2019-07-07 16:37:33 +0300

    Add some buildPatch method and private methods for it

--- include/PatchBuilder/PatchBuilder.h
+++ include/PatchBuilder/PatchBuilder.h
@@ -1,10 +1,23 @@
 #ifndef PATCHBUILDER_H
 #define PATCHBUILDER_H
-#include <DBProvider/DBProvider.h>
+
+#include <string>
+#include "DBProvider/DBProvider.h"
+
+using namespace std;
 
 class PatchBuilder {
 public:
-  PatchBuilder();
+  PatchBuilder(string pPatchListDirectory);
   ~PatchBuilder();
+  void buildPatch(string directory);
+
+private:
+	string patchListDirectory;
+
+	void createInstallPocket(string directory);
+	void createObjectList(string directory);
+	void createInstallScript(string directory);
+	void createDependencyList(string directory);
 };
 #endif
