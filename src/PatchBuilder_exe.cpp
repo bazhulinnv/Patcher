f034d1c07bc3d30d1bcd212985966e98241038e1
TimurSirkin <timur_sirkin@mail.ru>
2019-07-13 16:29:44 +0300

    Add creating of Install script and add parsing by template

--- src/PatchBuilder_exe.cpp
+++ src/PatchBuilder_exe.cpp
@@ -8,6 +8,6 @@ int main()
 {
 	// Temp logic
 	string tempDirectory = "C:\\Users\\Timur\\Documents\\Temp";
-	PatchBuilder patchBuilder(tempDirectory + "\\PatchList.txt");
+	PatchBuilder patchBuilder(tempDirectory + "\\PatchList.txt", tempDirectory + "\\Templates.ini", "doo", "Doors");
 	patchBuilder.buildPatch(tempDirectory);
 }
