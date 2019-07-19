20a92eaed9eaed9b325ed86c0c621562fd00ebbd
Vic Khov <victorKhovTko@yandex.ru>
2019-07-19 15:27:31 +0300

    Added printQueryResult() method and tested with objects from db

--- include/DBProvider/DBProvider.h
+++ include/DBProvider/DBProvider.h
@@ -9,6 +9,19 @@ typedef int Schema;
 typedef int ObjectName;
 typedef int ObjectType;
 
+struct StringSQLCollection
+{
+	std::string getObjects =
+			"SELECT /*sequences */\
+				f.sequence_schema AS obj_schema, f.sequence_name AS obj_name, 'sequence' AS obj_type\
+				from information_schema.sequences f\
+			UNION ALL\
+			SELECT /*tables */\
+				f.table_schema AS obj_schema, f.table_name AS obj_name, 'tables' AS obj_type\
+				from information_schema.tables f\
+			WHERE f.table_schema in('public', 'io', 'common', 'secure')";
+};
+
 class DBProvider
 {
 public:
@@ -16,6 +29,7 @@ public:
 	~DBProvider();
 	std::vector<std::tuple<Schema, ObjectName, ObjectType>> getObjects();
 	pqxx::result query(std::string strSQL);
+	void printQueryData(pqxx::result res);
 
 private:
 	DBConnection *conn = nullptr;
--- src/DBProvider.cpp
+++ src/DBProvider.cpp
@@ -1,6 +1,7 @@
 #include <DBProvider/DBProvider.h>
 #include <pqxx/pqxx>
 #include <pqxx/transaction>
+#include <iostream>
 
 DBProvider::DBProvider(std::string args)
 {
@@ -18,7 +19,7 @@ std::vector<std::tuple<Schema, ObjectName, ObjectType>> DBProvider::getObjects()
 	return std::vector<std::tuple<Schema, ObjectName, ObjectType>>();
 }
 
-pqxx::result DBProvider::query(std::string strSQL)
+pqxx::result DBProvider::query(std::string stringSQL)
 {	
 	// Connection must be already set
 	if (!conn->getConnection())
@@ -29,7 +30,23 @@ pqxx::result DBProvider::query(std::string strSQL)
 	pqxx::work trans(*conn->getConnection(), "trans");
 
 	// Get result from database
-	pqxx::result res = trans.exec(strSQL);
+	pqxx::result res = trans.exec(stringSQL);
 	trans.commit();
 	return res;
+}
+
+void DBProvider::printQueryData(pqxx::result queryResult)
+{
+	// Iterate over the rows in our result set.
+	// Results objects are containers similar to std::vector and such.
+	for (	pqxx::result::const_iterator row = queryResult.begin();
+			row != queryResult.end();
+			++row )
+	{
+		std::cout
+			<< row["obj_schema"].as<std::string>() << "\t"
+			<< row["obj_name"].as<std::string>() << "\t"
+			<< row["obj_type"].as<std::string>()
+			<< std::endl;
+	}
 }
