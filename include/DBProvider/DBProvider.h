a1007764aa3ad68031af8354d1a6a179bae01680
Vic Khov <victorKhovTko@yandex.ru>
2019-07-19 09:41:34 +0300

    Added DBProviderLogger to use as logger in DBProvider_test and also done small refactoring

--- include/DBProvider/DBProvider.h
+++ include/DBProvider/DBProvider.h
@@ -1,11 +1,24 @@
 #ifndef DBPROVIDER_H
 #define DBPROVIDER_H
 
-class DBProvider {
+#include "../include/DBProvider/DBConnection.h"
+#include <tuple>
+#include <pqxx/pqxx>
+
+typedef int Schema;
+typedef int ObjectName;
+typedef int ObjectType;
+
+class DBProvider
+{
 public:
-  DBProvider();
-  ~DBProvider();
-  int getObjects();
-  void connect();
+	explicit DBProvider(std::string args);
+	~DBProvider();
+	std::vector<std::tuple<Schema, ObjectName, ObjectType>> getObjects();
+	// pqxx::result query(std::string strSQL);
+
+private:
+	DBConnection *conn = nullptr;
 };
+
 #endif
