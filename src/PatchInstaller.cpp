bfdd721252ce0d7adebef1219696451baa33c65e
Kate <kate.vinnik393@gmail.com>
2019-07-25 15:38:34 +0300

    Added the treatment of edge cases (wrong input data)

--- src/PatchInstaller.cpp
+++ src/PatchInstaller.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <array>
 #include <time.h>
+#include <filesystem>
 #include <direct.h>
 
 #include "PatchInstaller/PatchInstaller.h"
@@ -35,26 +36,63 @@ bool PatchInstaller::checkObjectsForExistenceFromFile(std::string nameOfFile, DB
 #define __unix__
 #endif
 
+// You can use a simple guard class to make sure the buffer is always reset:
+struct cerrRedirect
+{
+	cerrRedirect(std::streambuf *new_buffer) : old(std::cerr.rdbuf(new_buffer)) {}
+	~cerrRedirect() { std::cerr.rdbuf(old); }
+private:
+	std::streambuf *old;
+};
+
+struct cout_redirect {
+	cout_redirect(std::streambuf * new_buffer)
+		: old(std::cout.rdbuf(new_buffer))
+	{ }
+
+	~cout_redirect() {
+		std::cout.rdbuf(old);
+	}
+
+private:
+	std::streambuf * old;
+};
+
+/*inline bool fileExists() {
+	ifstream f("Install.bat");
+	return f.good();
+}*/
+
 /** When the method starts, the dependency check is considered successful. */
 bool PatchInstaller::startInstallation(char* directory) {
+#if (defined(__WIN32__))
 	chdir(directory);
 	std::array<char, 128> buffer;
 	std::string result;
 
-#if (defined(__WIN32__))
+	std::stringstream awsomeLogBuffer;
+	std::stringstream awsomeLogInfoBuffer;
+	auto *customRedirect = new cerrRedirect(awsomeLogBuffer.rdbuf());
+	auto *customRedirect1 = new cout_redirect(awsomeLogInfoBuffer.rdbuf());
+	std::cerr << "ERROR" << std::endl;
+	std::cout << "INFO" << std::endl;
+	std::string awsomeErrors = awsomeLogBuffer.str();
+	std::string info = awsomeLogInfoBuffer.str();
+
 	FILE* pipe = _popen("Install.bat", "r");
 	if (!pipe)
 	{
 		std::cerr << "Couldn't start command." << std::endl;
-		return 0;
+		return false;
 	}
-	while (fgets(buffer.data(), 128, pipe) != NULL) {
-		result += buffer.data();
-	}
+	while (fgets(buffer.data(), 128, pipe) != NULL) {};
+	awsomeErrors = awsomeLogBuffer.str();
+	info = awsomeLogInfoBuffer.str();
+	std::cout << awsomeErrors << std::endl;
+	std::cout << "####" << info << "###" << std::endl;
+	delete customRedirect;
+	delete customRedirect1;
 	auto returnCode = _pclose(pipe);
-
-	std::cout << "!!!" << result << "!!" << std::endl;
-	//std::cout << returnCode << std::endl;
 #endif
 #if (defined(__unix__)) 
 	FILE* pipe = popen("Install.bat", "r");
--- src/PatchInstaller_exe.cpp
+++ src/PatchInstaller_exe.cpp
@@ -1,29 +1,69 @@
 #include <iostream>
 #include <fstream>
-
+#include <filesystem>
+#include <direct.h>
 #include "PatchInstaller/PatchInstaller.h"
 #include "PatchInstaller/DependenciesChecker.h"
 #include "PatchInstaller/FileParser.h"
 
+bool directoryExists(char* directory) {
+	const std::filesystem::path path(directory);
+	return std::filesystem::exists(directory);
+}
+
+inline bool fileExists(char* directory) {
+	chdir(directory);
+	ifstream f("Install.bat");
+	return f.good();
+}
+
 int main(int argc, char* argv[]) { 
 	std::string nameOfFile =  "DependencyList.dpn";
 	PatchInstaller patchInstaller;
-	// gui pass 0, if user want to check; pass 1, if user want to install
-	std::string parameters(argv[1]);
-	DBProvider *dbProvider = new DBProvider(parameters);
-	//bool ttt = patchInstaller.checkObjectsForExistenceFromFile(nameOfFile, *dbProvider);
-	if (strcmp(argv[2], "check") == 0)
-	{
-		//std::cout << "Start checking:\n";
-		patchInstaller.checkObjectsForExistenceFromFile(nameOfFile, *dbProvider);
+	if (argv[1] == nullptr) {
+		std::cout << "Please, enter database connection parameters. \n";
+		return -1;
 	}
-	if (strcmp(argv[2], "install") == 0)
-	{
-		//std::cout << installerDirectory;
-		std::cout << "start installation:";
-		//std::string installerDirectory(argv[3]);
-		patchInstaller.startInstallation(argv[3]);
+	else {
+		std::string parameters(argv[1]);
+		try {
+			DBProvider *dbProvider = new DBProvider(parameters);
+			if (argv[2] == nullptr || !(strcmp(argv[2], "check") == 0 || strcmp(argv[2], "install") == 0)) {
+				std::cout << "Wrong command of installer. Choose install/check. \n";
+				return -1;
+			}
+			else {
+				if (strcmp(argv[2], "check") == 0) {
+					patchInstaller.checkObjectsForExistenceFromFile(nameOfFile, *dbProvider);
+					return 0;
+				}
+				else if (strcmp(argv[2], "install") == 0) {
+					if (argv[3] == nullptr) { 
+						std::cout << "Please, enter the directory of installation script.\n";
+						return -1;
+					}
+					else {
+						if (!directoryExists(argv[3])) {
+							std::cout << "Directory does not exists or wrong.\n";
+							return -1;
+						}
+						else {
+							if (fileExists(argv[3])) {
+								patchInstaller.startInstallation(argv[3]);
+								return 0;
+							}
+							else {
+								std::cout << "Directory does not exists or wrong.\n";
+								return -1;
+							}
+						}
+					}
+				}
+			}
+		}
+		catch (std::terminate_handler/* Exception from DBProvider?*/) {
+			std::cout << "Parameters were wrong.\n";
+			return -1;
+		}
 	}
-	//delete dbProvider;
-	return 0;
 }
